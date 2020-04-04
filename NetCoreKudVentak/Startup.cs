69d59776c397f888bf98505202b9592927c81566
Ashish Kumar Agarwal <AshishKumar.Agarwal@csgi.com>
2020-04-04 20:41:02 +0530

    learned to retrive value from setting file- testing git ammend command here

--- NetCoreKudVentak/Startup.cs
+++ NetCoreKudVentak/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,12 @@ namespace NetCoreKudVentak
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,8 +39,8 @@ namespace NetCoreKudVentak
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
-                    await context.Response.WriteAsync(processName);
+                    //var processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                    await context.Response.WriteAsync(_config["TestKey"]);
                 });
             });
         }
